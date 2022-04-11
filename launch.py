bf0236e1b7370c4f86975b02f0841cbb4a742313
Juan <juan.sueiro@outlook.com>
2022-04-11 17:29:58 +0200

    added win10 desktop notification for break reminder

--- launch.py
+++ launch.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from tabulate import tabulate
 import sys
 from functools import partial
+from win10toast_click import ToastNotifier
 
 
 # think of requests as the browser who grabs the url
@@ -23,6 +24,10 @@ subtext2 = soup2.select('.subtext')
 
 all_links = links + links2
 all_subtexts = subtext + subtext2
+
+
+def launch_page():
+    webbrowser.open('index.html')
 
 
 def sort_stories_by_votes(hnlist):
@@ -57,11 +62,9 @@ fileout = open('html-table.html', 'w')
 
 html_body = """
 <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Coffe Break</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></head><body>
-
 """
 
 html_footer = """
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> <script src="js/main.js"></script> <script src="js/main.js"></script>
     <script>
         apply();
@@ -73,4 +76,8 @@ fileout.writelines(html_table)
 fileout.writelines(html_footer)
 fileout.close()
 
-webbrowser.open('index.html')
+
+# experimental
+toast = ToastNotifier()
+toast.show_toast('Time for a break?', 'You should go and get some coffee.',
+                 duration=30, callback_on_click=launch_page)
