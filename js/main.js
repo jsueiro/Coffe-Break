99cad27f5af3221e79ad5d75ce688bb2eaa46654
Juan <juan.sueiro@outlook.com>
2022-04-14 10:11:27 +0200

    added weather widget with API call to openweather

--- js/main.js
+++ js/main.js
@@ -27,8 +27,44 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
+
+
+const getlocation = () => {
+    navigator.geolocation.watchPosition(async (pos) => {
+        var lat = pos.coords.latitude;
+        var long = pos.coords.longitude;
+        var wkey = config.W_KEY;
+
+        const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=' + wkey + '&units=metric'
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setWeather(data);
+
+    });
+}
+
+function setWeather(data) {
+
+    console.log(data);
+
+    loc = document.querySelector('#place');
+    loc.textContent = data.name + ', ' + data.sys.country;
+
+    weather = document.querySelector('#weather');
+    weather.textContent = data.weather[0].description;
+
+    temp = document.querySelector('#temp');
+    temp.textContent = data.main.temp + ' C°';
+
+    document.getElementById("wimg").src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + '.png';
+
+
+}
+
 window.onload = function () {
     var fiveMinutes = 60 * 5,
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
+    getlocation();
 };
